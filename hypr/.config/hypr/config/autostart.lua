54d709bbcd1a61e94ed6ece726640e23c880d5f5
nleloc <locleluanluc@gmail.com>
2026-05-25 22:39:50 +0700

    idk

--- hypr/.config/hypr/config/autostart.lua
+++ hypr/.config/hypr/config/autostart.lua
@@ -7,6 +7,9 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 	hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 16")
 	hl.exec_cmd("vicinae server & kitty")
+	-- don't ask me why about this
+	hl.dispatch(hl.dsp.cursor.move({ x = 1093, y = 576 }))
+	--
 	hl.exec_cmd("fcitx5 & systemctl --user start hyprpolkitagent & amixer -c 0 set 'Internal Mic Boost' 53%")
 	hl.exec_cmd("qs -c caelestia")
 end)
