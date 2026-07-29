c0e41abaa8778c29d273616414e6d81618d833f8
nleloc <locleluanluc@gmail.com>
2026-07-29 21:06:54 +0700

    fix (rqs.sh): kill wlsunset too, avoid nightlight doesn't work after restart qs

--- misc/rqs.sh
+++ misc/rqs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-killall -q qs quickshell
+killall -q qs quickshell wlsunset
 
-while pgrep -u $USER -x "qs|quickshell" >/dev/null; do # Check if quickshell die yet
+# check if quickshell and its shit die yet
+while pgrep -u $USER -x "qs|quickshell|wlsunset" >/dev/null; do
   sleep 0.2
 done
 qs -c noctalia-shell &
